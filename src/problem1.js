c8f8c06693f7ea402d7837738dc14eb3dee245d0
PARK-CHANUK <doosan1224@naver.com>
2023-09-05 22:16:18 +0900

    feat: exceptionHandler 함수 기능구현
    예외처리기능을 담당하는 exceptionHandler 함수를 만들었습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -6,9 +6,6 @@ function problem1(pobi, crong) {
 module.exports = problem1;
 
 function getScore(page) {
-  // 입력 배열의 요소가 숫자이기 때문에 string 타입으로 변환
-  // 스프레드로 나눠준다
-  // reduce를 이용해서 합을 구한다
   const leftSum = [...(page[0] + '')].reduce((acc, cul) => Number(cul) + acc, 0);
   const rightSum = [...(page[1] + '')].reduce((acc, cul) => Number(cul) + acc, 0);
   const leftMultiply = [...(page[0] + '')].reduce((acc, cul) => Number(cul) * acc, 1);
@@ -17,4 +14,10 @@ function getScore(page) {
   const result = [leftSum, rightSum, leftMultiply, rightMultiply];
   result.sort((a, b) => a - b);
   return result[result.length - 1];
+}
+
+function exceptionHandler(page) {
+  if (page === [1, 2] || page === [399, 400]) return false;
+  else if (page[1] - page[0] !== 1) return false;
+  else return true;
 }
