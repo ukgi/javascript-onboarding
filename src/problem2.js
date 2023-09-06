042eaa6ef33da088daf70f976280d95ed4dfa516
PARK-CHANUK <doosan1224@naver.com>
2023-09-06 18:46:58 +0900

    feat: 기능구현완료

--- src/problem2.js
+++ src/problem2.js
@@ -2,10 +2,10 @@ function problem2(cryptogram) {
   let isContinuous = true;
   const arr = [...cryptogram];
   while (isContinuous) {
-    isContinuous = false; // 초기에 false로 설정
-    arr.forEach((char, i) => {
+    isContinuous = false;
+    arr.forEach((_char, i) => {
       if (arr[i] === arr[i + 1]) {
-        isContinuous = true; // 두 문자가 같으면 true로 설정
+        isContinuous = true;
         arr.splice(i, 2);
       }
     });
@@ -14,5 +14,3 @@ function problem2(cryptogram) {
 }
 
 module.exports = problem2;
-
-console.log(problem2('browoanoommnaon'));
