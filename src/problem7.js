fb07c43fe553b4183f00127d19e5028e146b3111
PARK-CHANUK <doosan1224@naver.com>
2023-09-11 14:02:59 +0900

    feat: checkMutualFriends 함수 기능구현 함께아는 친구 목록을 반환하는 함수를 만들었습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,15 +1,31 @@
 function problem7(user, friends, visitors) {
-  return checkUserFriends(user, friends);
+  const userFriends = checkUserFriends(user, friends);
+  return checkMutualFriends(user, userFriends, friends);
 }
 
 function checkUserFriends(user, friends) {
-  const friendList = friends.reduce((acc, cur) => {
+  const friendListWithUser = friends.reduce((acc, cur) => {
     if (cur.includes(user)) {
       acc.push(cur);
     }
     return acc;
   }, []);
-  return friendList.flat(2).filter((u) => u !== user);
+  return friendListWithUser.flat(2).filter((u) => u !== user);
+}
+
+function checkMutualFriends(user, userFriends, friends) {
+  const mutualFriendsWithUserFriends = [];
+  friends.forEach((friendList) => {
+    userFriends.forEach((friend) => {
+      if (friendList.includes(friend) && !friendList.includes(user)) {
+        mutualFriendsWithUserFriends.push(friendList);
+      }
+    });
+  });
+  const mutualFriends = mutualFriendsWithUserFriends.flat(2).reduce((acc, cur) => {
+    !userFriends.includes(cur) ? acc.push(cur) : acc;
+    return acc;
+  }, []);
 }
 
 module.exports = problem7;
