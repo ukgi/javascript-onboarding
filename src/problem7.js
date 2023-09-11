3c959037d3cb02b79cb73acae48978c5b6ba7442
PARK-CHANUK <doosan1224@naver.com>
2023-09-11 13:37:09 +0900

    feat: checkUserFriends 함수 기능구현 friends 목록 중 user와 친구인 사람들을 배열로 저장해서 리턴하는 함수를 만들었습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,30 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  return checkUserFriends(user, friends);
+}
+
+function checkUserFriends(user, friends) {
+  const friendList = friends.reduce((acc, cur) => {
+    if (cur.includes(user)) {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+  return friendList.flat(2).filter((u) => u !== user);
 }
 
 module.exports = problem7;
+
+console.log(
+  problem7(
+    'mrko',
+    [
+      ['donut', 'andole'],
+      ['donut', 'jun'],
+      ['donut', 'mrko'],
+      ['shakevan', 'andole'],
+      ['shakevan', 'jun'],
+      ['shakevan', 'mrko'],
+    ],
+    ['bedi', 'bedi', 'donut', 'bedi', 'shakevan']
+  )
+);
