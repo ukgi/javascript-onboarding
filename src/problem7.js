6319ecced8bca326914eca35bafcaa21119b52dd
PARK-CHANUK <doosan1224@naver.com>
2023-09-11 18:24:39 +0900

    feat: combineScore 함수와 sortArray 함수 기능구현 함께 아는 친구가 있으면서 방문자인 경우 점수를 합쳐주는 기능을 하는 함수와 조건에 맞게 배열을 정렬하는 함수를 구현했습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,8 +1,11 @@
 function problem7(user, friends, visitors) {
   const userFriends = checkUserFriends(user, friends);
   const mutualFriends = checkMutualFriends(user, userFriends, friends);
-  console.log(makeObjectFriends(mutualFriends, 'mutualFriend'));
-  console.log(makeObjectFriends(visitors, 'visitor'));
+  const visitorList = checkVisitor(userFriends, visitors);
+  const combineMutualAndVisitor = combineScores(
+    makeObjectFriends(mutualFriends, 'mutualFriend').concat(makeObjectFriends(visitorList, 'visitor'))
+  );
+  return sortArray(combineMutualAndVisitor);
 }
 
 function checkUserFriends(user, friends) {
@@ -31,37 +34,74 @@ function checkMutualFriends(user, userFriends, friends) {
   return mutualFriends;
 }
 
+function checkVisitor(userFriends, visitors) {
+  const visitorList = [];
+  visitors.forEach((visitor) => {
+    if (!userFriends.includes(visitor)) {
+      visitorList.push(visitor);
+    }
+  });
+  return visitorList;
+}
+
+// ✅
 function makeObjectFriends(friends, option) {
   return friends.reduce((acc, cur) => {
     let found = false;
     const updatedAcc = acc.map((friend) => {
       if (friend.name === cur) {
         found = true;
-        return (option = 'visitor' ? { ...friend, score: friend.score + 1 } : { ...friend, score: friend.score + 10 });
+        return option === 'visitor' ? { ...friend, score: friend.score + 1 } : { ...friend, score: friend.score + 10 };
       }
       return friend;
     });
 
     if (!found) {
-      option = 'visitor' ? updatedAcc.push({ name: cur, score: 1 }) : updatedAcc.push({ name: cur, score: 10 });
+      option === 'visitor' ? updatedAcc.push({ name: cur, score: 1 }) : updatedAcc.push({ name: cur, score: 10 });
     }
     return updatedAcc;
   }, []);
 }
 
+// ✅
+function combineScores(arr) {
+  const result = [];
+  const scoreMap = {};
+  arr.forEach(({ name, score }) => {
+    if (scoreMap.hasOwnProperty(name)) {
+      scoreMap[name] += score;
+    } else {
+      scoreMap[name] = score;
+    }
+  });
+
+  for (const name in scoreMap) {
+    result.push({ name: name, score: scoreMap[name] });
+  }
+
+  return result;
+}
+
+function sortArray(arr) {
+  return arr
+    .sort()
+    .sort((a, b) => b['score'] - a['score'])
+    .map((user) => user.name);
+}
+
 module.exports = problem7;
 
-console.log(
-  problem7(
-    'mrko',
-    [
-      ['donut', 'andole'],
-      ['donut', 'jun'],
-      ['donut', 'mrko'],
-      ['shakevan', 'andole'],
-      ['shakevan', 'jun'],
-      ['shakevan', 'mrko'],
-    ],
-    ['bedi', 'bedi', 'donut', 'bedi', 'shakevan']
-  )
-);
+// console.log(
+//   problem7(
+//     'mrko',
+//     [
+//       ['donut', 'andole'],
+//       ['donut', 'jun'],
+//       ['donut', 'mrko'],
+//       ['shakevan', 'andole'],
+//       ['shakevan', 'jun'],
+//       ['shakevan', 'mrko'],
+//     ],
+//     ['bedi', 'bedi', 'donut', 'bedi', 'shakevan']
+//   )
+// );
