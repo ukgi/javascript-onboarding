c7ff522bdf953e431408b81967f79db3ec4bac83
PARK-CHANUK <doosan1224@naver.com>
2023-09-11 16:16:23 +0900

    feat: makeObjectFriends 함수 기능구현 user와 친구인 사람들과 친구인 목록들 즉, 함께 아는 친구 목록을 파악해서 각각 이름과 점수를 가지고있는 객체로 만들어주는 함수를 구현했습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,7 @@
 function problem7(user, friends, visitors) {
   const userFriends = checkUserFriends(user, friends);
-  return checkMutualFriends(user, userFriends, friends);
+  const mutualFriends = checkMutualFriends(user, userFriends, friends);
+  return makeObjectFriends(mutualFriends);
 }
 
 function checkUserFriends(user, friends) {
@@ -25,6 +26,25 @@ function checkMutualFriends(user, userFriends, friends) {
   const mutualFriends = mutualFriendsWithUserFriends.flat(2).reduce((acc, cur) => {
     !userFriends.includes(cur) ? acc.push(cur) : acc;
     return acc;
+  }, []);
+  return mutualFriends;
+}
+
+function makeObjectFriends(friends) {
+  return friends.reduce((acc, cur) => {
+    let found = false;
+    const updatedAcc = acc.map((friend) => {
+      if (friend.name === cur) {
+        found = true;
+        return { ...friend, score: friend.score + 10 };
+      }
+      return friend;
+    });
+
+    if (!found) {
+      updatedAcc.push({ name: cur, score: 10 });
+    }
+    return updatedAcc;
   }, []);
 }
 
